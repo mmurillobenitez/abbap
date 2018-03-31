81e22bcc1acf09024ed1100d2b720c87fff295b6
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 12:30:49 +0200

    Update dropMarca.php

--- dropMarca.php
+++ dropMarca.php
@@ -13,7 +13,7 @@ $dbname = "u426573260_primo";
 
 $id= $_POST["id"];
 
-try {
+
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -25,11 +25,7 @@ try {
    include 'homeU.php';
 
     
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
+    
 
 $conn = null;
 ?> 
