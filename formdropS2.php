b4db7f24b2abbdd64cb176988c82f821797e3047
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 13:03:01 +0200

    Update formdropS2.php

--- formdropS2.php
+++ formdropS2.php
@@ -101,15 +101,12 @@ span.psw {
 
     <?php
      
-$servername = "mysql.hostinger.it";
-$username = "u426573260_luis";
-$password = "cinegga00";
-$dbname = "u426573260_primo";
+
 
 $codU = $_SESSION['id'] ;
 
 
-$cn = mysqli_connect($servername, $username, $password, $dbname);
+$cn = mysqli_connect("mysql.hostinger.it", "u426573260_luis", "cinegga00", "u426573260_primo");
 $sql = "SELECT *  FROM dispositivo ";
 $ris = mysqli_query($cn,$sql) ; //esegue la query
 $n_ris = mysqli_num_rows($ris); //dice il numero di record trovati
