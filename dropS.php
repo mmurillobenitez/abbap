944f649945da3d38be35c02848ff3ed807ad5fa5
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 13:38:39 +0200

    Update dropS.php

--- dropS.php
+++ dropS.php
@@ -6,15 +6,12 @@ session_start();
 <body>
     
      <?php
-$servername = "mysql.hostinger.it";
-$username = "u426573260_luis";
-$password = "cinegga00";
-$dbname = "u426573260_primo";
+
 
 $idU= $_POST["id"];
 
+$conn = new PDO("mysql:host="mysql.hostinger.it";dbname="u426573260_primo", "u426573260_luis", "cinegga00");
 
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "DELETE FROM dispositivo WHERE id = '$idU' ";
@@ -25,8 +22,7 @@ $idU= $_POST["id"];
    include 'homeU.php';
 
     
-    
-$conn = null;
+
 ?> 
 <br>
 <br>
