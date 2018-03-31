416b38a012df83c5df0e974bbbd76d26f79ac31a
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 12:31:21 +0200

    Update dropMS.php

--- dropMS.php
+++ dropMS.php
@@ -15,7 +15,7 @@ $id= $_POST['id'];
 
 
 
-try {
+
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -27,11 +27,7 @@ try {
   
 
     
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
+
 
 $conn = null;
 include'homeU.php';
