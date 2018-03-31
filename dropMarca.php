a046d84149afe049af1d62be36eb6b9ed7974073
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 13:38:05 +0200

    Update dropMarca.php

--- dropMarca.php
+++ dropMarca.php
@@ -6,15 +6,12 @@ session_start();
 <body>
     
      <?php
-$servername = "mysql.hostinger.it";
-$username = "u426573260_luis";
-$password = "cinegga00";
-$dbname = "u426573260_primo";
+
 
 $id= $_POST["id"];
 
+$conn = new PDO("mysql:host="mysql.hostinger.it";dbname="u426573260_primo", "u426573260_luis", "cinegga00");
 
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "DELETE FROM marca WHERE id = '$id' ";
@@ -27,7 +24,7 @@ $id= $_POST["id"];
     
     
 
-$conn = null;
+
 ?> 
 <br>
 <br>
