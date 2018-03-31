04e40d410ecd34ccb0864f18b92e0d5c50911111
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 12:31:51 +0200

    Update dropMR.php

--- dropMR.php
+++ dropMR.php
@@ -15,7 +15,7 @@ $idR= $_POST['idR'];
 $valoreR= $_POST["valoreR"];
 
 
-try {
+
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -27,11 +27,7 @@ try {
    include 'homeU.php';
 
     
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
+ 
 
 $conn = null;
 ?> 
