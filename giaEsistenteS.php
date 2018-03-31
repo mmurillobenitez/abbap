bfdc041d947b831bcce4a2c80f04aaac4e00d52a
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 12:26:22 +0200

    Update giaEsistenteS.php

--- giaEsistenteS.php
+++ giaEsistenteS.php
@@ -21,7 +21,7 @@ $id =  $_POST["user"];
 $descrizione =  $_POST["ide"];
 
  
-try {
+
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -33,11 +33,8 @@ try {
     include 'homeU.php';
     
     
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
+    
+
 
 $conn = null;
 ?> 
