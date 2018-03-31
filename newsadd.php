b9c832e228487997d161703e6ae018c61e63c440
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 12:23:10 +0200

    Update newsadd.php

--- newsadd.php
+++ newsadd.php
@@ -22,7 +22,7 @@ $tipo =  $_POST["tipo"];
 $marca =  $_POST['marca'];
 $p = $_POST['p'];
  
-try {
+
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -34,11 +34,7 @@ try {
    include 'homeU.php';
     
     
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
+  
 
 $conn = null;
 ?> 
