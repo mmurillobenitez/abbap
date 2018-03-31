7347eda68d9a024f00b4c801d9981cfda4b84858
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 12:59:23 +0200

    Update allU.php

--- allU.php
+++ allU.php
@@ -13,15 +13,12 @@ if ($_SESSION['livello']>1 ){
     }
     
     
-$servername = "mysql.hostinger.it";
-$username = "u426573260_luis";
-$password = "cinegga00";
-$dbname = "u426573260_primo";
+
 
 $codU = $_SESSION['id'] ;
 
 
-$cn = mysqli_connect($servername, $username, $password, $dbname);
+$cn = mysqli_connect("mysql.hostinger.it", "u426573260_luis", "cinegga00", "u426573260_primo");
 $sql = "SELECT *  FROM Utente ";
 $ris = mysqli_query($cn,$sql) ; //esegue la query
 $n_ris = mysqli_num_rows($ris); //dice il numero di record trovati
