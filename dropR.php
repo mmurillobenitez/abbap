064ab01fcdb3c0d0e0212afda5f459d47b95658b
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-03-31 13:30:04 +0200

    Update dropR.php

--- dropR.php
+++ dropR.php
@@ -6,17 +6,14 @@ session_start();
 <body>
     
      <?php
-$servername = "mysql.hostinger.it";
-$username = "u426573260_luis";
-$password = "cinegga00";
-$dbname = "u426573260_primo";
+
 
 $idR= $_POST['idR'];
 $valoreR= $_POST["valoreR"];
 
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn = new PDO("mysql:host="mysql.hostinger.it";dbname="u426573260_primo", "u426573260_luis", "cinegga00");
+
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "DELETE FROM rilevazione WHERE codice_S = '$idR' and valore = '$valoreR' ";
@@ -27,13 +24,9 @@ try {
    include 'homeU.php';
 
     
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
 
-$conn = null;
+
+
 ?> 
 <br>
 <br>
