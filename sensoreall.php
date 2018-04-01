29ae29bd8932892b7b912dd7d7e72f8a8335e89c
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-04-01 20:00:13 +0200

    Update sensoreall.php

--- sensoreall.php
+++ sensoreall.php
@@ -34,13 +34,24 @@ if ($n_ris> 0){
     while ($row = $ris->fetch_assoc()) {
     // output data of each row
     echo " Sensore: <br>";
-     echo "-Id: ".$row['id']." - Tipo : ".$row["tipo"]." -Marca  ".$row['marca']." - Zona ".$row['zona ']."  Codice Utente  ".$row['proprieta']." <br>";
+     echo "Id: ".$row['id']." - Tipo : ".$row["tipo"]." Marca  ".$row['marca']."  Zona ".$row['zona ']."  Codice Utente  ".$row['proprieta']." <br>";
     
        
        
     }
     
- }
+ }else {
+     echo" Non ci sono sensori ";
+	 
+}
+ 
+
+   
+   
+   
+    
+
+$cn = null;
  
 
    
