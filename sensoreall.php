f2e0ab67e793473551eb3bd03c278afa4a2f9643
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-04-01 14:35:44 +0200

    Update sensoreall.php

--- sensoreall.php
+++ sensoreall.php
@@ -7,8 +7,7 @@ session_start();
 <html>
 <title>W3.CSS</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <body>
    
 <?php
@@ -41,10 +40,7 @@ if ($n_ris> 0){
        
     }
     
- }else {
-     
-	 
-}
+ }
  
 
    
@@ -52,7 +48,7 @@ if ($n_ris> 0){
    
    
 
-$cn = null;
+
 
 ?> 
 
