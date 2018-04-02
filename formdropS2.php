b3ec6d915ecf06581a1aa4894b7bafd151e02ef0
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-04-02 10:42:42 +0200

    Update formdropS2.php

--- formdropS2.php
+++ formdropS2.php
@@ -106,8 +106,8 @@ span.psw {
 $codU = $_SESSION['id'] ;
 
 
-$cn = mysqli_connect("mysql.hostinger.it", "u426573260_luis", "cinegga00", "u426573260_primo");
-$sql = "SELECT *  FROM dispositivo ";
+$cn = mysqli_connect('mysql.hostinger.it', 'u426573260_luis', 'cinegga00', 'u426573260_primo');
+$sql = 'SELECT *  FROM dispositivo ';
 $ris = mysqli_query($cn,$sql) ; //esegue la query
 $n_ris = mysqli_num_rows($ris); //dice il numero di record trovati
 
@@ -120,7 +120,7 @@ if ($n_ris> 0){
     while ($row = $ris->fetch_assoc()) {
     // output data of each row
     
-     echo "<p> Id: ".$row['id']."  Tipo  ".$row["tipo"]." Marca  ".$row['marca']."  Zona ".$row['zona ']."  Codice Utente  ".$row['proprieta']." </p>";
+     echo " Id: ".$row['id']."  Tipo  ".$row['tipo']." Marca  ".$row['marca']."  Zona ".$row['zona ']."  Codice Utente  ".$row['proprieta']." ";
     
        
        
