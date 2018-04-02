a753f1511fb11b373213c68e2b9c7e82932e4a4a
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-04-02 10:40:46 +0200

    Update tipoS.php

--- tipoS.php
+++ tipoS.php
@@ -19,9 +19,8 @@ if ($_SESSION['livello']>1 ){
 $codU = $_SESSION['id'] ;
 
 
-$cn = mysqli_connect("mysql.hostinger.it", "u426573260_luis", "cinegga00", "u426573260_primo");
-$sql = " SELECT  tipologia , primaP, secondaP, terzaP
-        FROM  tipo ";
+$cn = mysqli_connect('mysql.hostinger.it', 'u426573260_luis', 'cinegga00', 'u426573260_primo');
+$sql = " SELECT  tipologia , primaP, secondaP, terzaP FROM  tipo ";
         
      
         
@@ -34,12 +33,12 @@ $n_ris = mysqli_num_rows($ris); //dice il numero di record trovati
 
    
 if ($n_ris > 0){
-    echo " Lista tipi dei   sensori : <br>";
+    echo " Lista tipi dei   sensori  <br>";
    
     while ($row = $ris->fetch_assoc()) {
     // output data of each row
     
-    echo " <p>Tipologia sensore   ".$row['tipologia']." Tipo valore stringa       ".$row['primaP']."   ".$row['secondaP']."  ".$row['terzaP']." </p>";
+    echo " <p>Tipologia sensore  ".$row['tipologia']." Tipo valore stringa       ".$row['primaP']."   ".$row['secondaP']."  ".$row['terzaP']." </p>";
      
     }
     
