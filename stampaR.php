c1b1d1b6ee56cc94be4124d1a6b22b7e3fbe3479
mmurillobenitez <37953731+mmurillobenitez@users.noreply.github.com>
2018-04-02 10:10:22 +0200

    Update stampaR.php

--- stampaR.php
+++ stampaR.php
@@ -39,7 +39,7 @@ if ($n_ris > 0){
     while ($row = $ris->fetch_assoc()) {
     // output data of each row
     
-    echo "Id sensore : ".$row['codice_S']."  Valore ".$row['valore']."  Descrizione ".$row['descrizione']."<br>";
+    echo "Id sensore  ".$row['codice_S']."  Valore ".$row['valore']."  Descrizione ".$row['descrizione']."<br>";
      
     }
     
